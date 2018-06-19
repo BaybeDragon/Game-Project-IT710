e11c8eb932c3724169bc52d1b53a562a4199cdc2
DashDarkfighter <39894504+DashDarkfighter@users.noreply.github.com>
2018-06-20 11:46:23 +1200

    this should push now

---
+++ Game-Project-IT710/Another Monday/Assets/_Scripts/BossScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossScript : MonoBehaviour {
+
+    public Image endGame;
+    public Image disret;
+
+    float health = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(health <= 0)
+        {
+            disret.enabled = false;
+            endGame.enabled = true;
+            Time.timeScale = 0f;
+			Application.Quit ();
+        }
+	}
+
+
+
+    public void TakeDamage(int dmg)
+    {
+        health -= dmg;
+
+    }
+}
---
+++ Game-Project-IT710/Another Monday/Assets/_Scripts/endlevel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class endlevel : MonoBehaviour
+{
+
+    int killCount = 0;
+    public int killAmount;
+    public Text killMore;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddKill()
+    {
+        killCount += 1;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (killCount >= killAmount)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {
+                killMore.enabled = true;
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            killMore.enabled = false;
+
+        }
+
+    }
+}
