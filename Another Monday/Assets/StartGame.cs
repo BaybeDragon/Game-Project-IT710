732b30c9c9be0df781231ae3e72ec4efdb6c8e01
DashDarkfighter <39894504+DashDarkfighter@users.noreply.github.com>
2018-06-21 16:17:20 +1200

    Finalthings

--- Another Monday/Assets/StartGame.cs
+++ Another Monday/Assets/StartGame.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class StartGame : MonoBehaviour {
 
     public Image blackscreen;
+	public Text starttext;
+	private AudioSource introvoice;
 
 	// Use this for initialization
 	void Start () {
         Time.timeScale = 0f;
-
+		starttext.enabled = true;
+		introvoice = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
@@ -19,8 +22,10 @@ public class StartGame : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+			introvoice.Stop ();
             Time.timeScale = 1f;
             Destroy(blackscreen);
+			starttext.enabled = false;
             Destroy(this);
         }
     }
